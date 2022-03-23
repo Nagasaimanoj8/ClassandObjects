6a066fdfe74f1c743732a7dcc17c5916ed74f36c
Nagasaimanoj8 <101272258+Nagasaimanoj8@users.noreply.github.com>
2022-03-23 06:44:44 +0530

    Add files via upload

---
+++ Inheritance/Dog Inheritance/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dog_Inheritance
+{
+    public class Animal
+    {
+        public void eat() { Console.WriteLine("Eating..."); }
+    }
+    public class Dog : Animal
+    {
+        public void bark() { Console.WriteLine("Barking..."); }
+    }
+}
